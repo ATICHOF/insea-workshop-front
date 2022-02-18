a1cdb99599b6364106345404f0e57ec83dd5d8eb
niimachigr <niimachigr@gmail.com>
2022-02-18 17:53:54 +0100

    add empty Page Component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import {BrowserRouter as Routes, Route} from 'react-router-dom';
 import './App.css';
-import EmptyCard from './components/ui/EmptyCard';
-import Sidebar from './components/ui/Sidebar';
 
 function App() {
   return (
     <div className="App">
-    <EmptyCard/>
+  
     </div>
     
   );
