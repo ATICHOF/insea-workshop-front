65dc527e5e6fa24dd25ccb5f9e6aee45eecb2bc9
niimachigr <niimachigr@gmail.com>
2022-02-18 21:55:16 +0100

    add apps.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import VideoCard from './components/ui/VideoCard';
 import Updatevid from './containers/Updatevid';
 const App = () => {
   return (
 
     <div>
-
+      <VideoCard/>
     </div>
 
   )
