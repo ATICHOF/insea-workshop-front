d60502bfb9941ee5652c622bca3253e40a902480
niimachigr <niimachigr@gmail.com>
2022-02-18 16:30:06 +0100

    add emptyCard

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Routes, Route} from 'react-router-dom';
 import './App.css';
+import EmptyCard from './components/ui/EmptyCard';
 import Sidebar from './components/ui/Sidebar';
 
 function App() {
   return (
-    <>
-    <Router>
-      <Sidebar/>
-      
-    </Router>
-    </>
+    <div className="App">
+    <Routes>
+     
+        <Route path="/" component={EmptyCard} />
+    </Routes>
+    </div>
+    
   );
 }
 
---
+++ src/components/ui/EmptyCard.js
@@ -0,0 +1,11 @@
+import Sidebar from "./Sidebar";
+
+const EmptyCard= () => {
+    return ( 
+        <div>
+            
+        </div>
+     );
+}
+ 
+export default EmptyCard;
