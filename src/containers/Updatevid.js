f20fe72a2e5aed004ec8495f9b7cdf66714d96cb
ATICHOF <sof.atiche@gmail.com>
2022-02-18 18:38:18 +0100

    message

--- src/containers/Updatevid.js
+++ src/containers/Updatevid.js
@@ -3,7 +3,7 @@ import "../styles/upload.css"
 const Updatevid = () => {
   return (
     <div className='updatevid'>
-      <button ><img src={require('./../assets/icons/Close.png')} alt="Close" className='close'/></button>
+      <button ><img src={require('../assets/icons/Close.png')} alt="Close" className='close'/></button>
       <h1 className='head'>Upload New Video</h1>
       <div className='Drop'></div>
     </div>
