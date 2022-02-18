12b4778be0132d7fae5fec40545d80464c5930d2
niimachigr <niimachigr@gmail.com>
2022-02-18 21:49:40 +0100

    add video card component

--- src/components/ui/VideoCard.js
+++ src/components/ui/VideoCard.js
@@ -1,1 +1,17 @@
- 
+import '../../styles/VideoCard.css';
+
+const VideoCard = () => {
+    return ( 
+    <div className="videoItem">
+        <img className="img" src={require("../../assets/images/thumbnail.png")} alt=''/>
+        <div className="video-content">
+            <div className="video-title"><h2>Sophia's Exclusive Birthday Photoshoot</h2></div>
+            <div className='btn'>
+                <button className='btn-edit'>Edit</button>
+                <button className='btn-delete'>Delete</button>
+            </div>
+        </div>
+    </div>
+    );
+}
+export default VideoCard;
