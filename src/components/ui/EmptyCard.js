a31fb0349500377f2d5b0ee76df145ff75d77637
niimachigr <niimachigr@gmail.com>
2022-02-18 18:49:31 +0100

    remove empty.png from public folder

--- src/components/ui/EmptyCard.js
+++ src/components/ui/EmptyCard.js
@@ -4,7 +4,7 @@ const EmptyCard= () => {
         <div className="empty-card">
             <img className="img" src={require("../../assets/images/empty.png")}  alt=""/>
             <h1>You have no video!</h1>
-            <p>Sorry, you don't have any video yet. Click add video to upload new one!.</p>
+            <p>Sorry, you don't have any video yet.Click add video to upload new one!.</p>
         </div>
         
     );
