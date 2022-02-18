896008aad230c6eecf0da5543250b7e79d69d6a5
ATICHOF <sof.atiche@gmail.com>
2022-02-18 18:12:34 +0100

    drop video container

---
+++ src/containers/Updatevid.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import "../styles/upload.css"
+const Updatevid = () => {
+  return (
+    <div className='updatevid'>
+      <button ><img src={require('./../assets/icons/Close.png')} alt="Close" className='close'/></button>
+      <h1 className='head'>Upload New Video</h1>
+      <div className='Drop'></div>
+    </div>
+  )
+}
+
+export default Updatevid
