07cd1c01b72165dc07be284716ed0d9bdc35ac4b
ATICHOF <sof.atiche@gmail.com>
2022-02-20 13:58:31 +0100

    modify modal styles

--- src/containers/Updatevid.js
+++ src/containers/Updatevid.js
@@ -4,11 +4,11 @@ const Updatevid = () => {
 
   return (
     <div className='updatevid'>
-      <a className='close' color='#FFF' href='/home'><img src={require('../assets/icons/Close.png')} alt="close"/></a>
+      <a className='closet' color='#FFF' href='/home'><img src={require('../assets/icons/Close.png')} alt="close"/></a>
       <h1 className='head'>Upload New Video</h1>
       <div className='Drop'>
-        <img src={require('../assets/icons/Video.png')} alt="video" className='video' />
-        <h1 className='p1'>Drag and drop video files to upload</h1>
+        <img src={require('../assets/icons/Video.png')} alt="video" className='videoicon' />
+        <label className='Dnd'>Drag and drop video files to upload</label>
         <p>Choose a PNG, JPG, or GIF file from your device</p>
         <button className='selectbtn'>Select a file</button>
       </div>
