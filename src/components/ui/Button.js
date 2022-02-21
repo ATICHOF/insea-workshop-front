e0ddb035340428a3763cb6ab27fdf5fc2a94fbfc
ATICHOF <sof.atiche@gmail.com>
2022-02-22 00:44:04 +0100

    modification cotinue

--- src/components/ui/Button.js
+++ src/components/ui/Button.js
@@ -2,11 +2,11 @@ import React from "react";
 import "../../styles/component.css";
 
 const Button = ({ textbtn, ttype }) => {
-	return (
-		<button className="btn" type={ttype}>
-			{textbtn}
-		</button>
-	);
+  return (
+    <button className="btn" type={ttype}>
+      {textbtn}
+    </button>
+  );
 };
 
 export default Button;
--- src/components/ui/input.js
+++ src/components/ui/input.js
@@ -2,23 +2,23 @@ import React from "react";
 
 import "../../styles/component.css";
 const Input = ({ name, ttype, valuee, onchangnt }) => {
-	const handleOnChange = (e) => {
-		if (onchangnt) {
-			onchangnt(e.target.value);
-		}
-		return null;
-	};
-	return (
-		<div className="input">
-			<label>{name}</label>
-			<input
-				className="txtinput"
-				type={ttype}
-				value={valuee}
-				onChange={handleOnChange}
-			></input>
-		</div>
-	);
+  const handleOnChange = (e) => {
+    if (onchangnt) {
+      onchangnt(e.target.value);
+    }
+    return null;
+  };
+  return (
+    <div className="input">
+      <label>{name}</label>
+      <input
+        className="txtinput"
+        type={ttype}
+        value={valuee}
+        onChange={handleOnChange}
+      ></input>
+    </div>
+  );
 };
 
 export default Input;
